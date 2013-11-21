7fc386182c94f52a88ea27d67282f07f7f748022
Nick DiRienzo <nvdirien@buffalo.edu>
2013-11-21 02:03:47 -0500

    Basic edit texts and buttons

--- src/edu/buffalo/cse/blue/pocketmocker/MainActivity.java
+++ src/edu/buffalo/cse/blue/pocketmocker/MainActivity.java
@@ -1,8 +1,12 @@
 package edu.buffalo.cse.blue.pocketmocker;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +21,14 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void submit(View view) {
+		Log.v("PM", "hi");
+		TextView mockedLat = (TextView)findViewById(R.id.edit_lat);
+		TextView mockedLong = (TextView)findViewById(R.id.edit_long);
+		String toDisplay = "Lat: " + mockedLat.getText() + " Long: " + mockedLong.getText();
+		Toast.makeText(getApplicationContext(), toDisplay, Toast.LENGTH_SHORT).show();
 	}
 
 }
