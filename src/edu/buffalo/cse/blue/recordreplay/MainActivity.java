ba31573036c37dd2ac9fbf2a9c5b30e3c513c8a1
Nick DiRienzo <me@nickdirienzo.com>
2014-01-08 01:21:33 -0500

    Record button switches state.

--- src/edu/buffalo/cse/blue/recordreplay/MainActivity.java
+++ src/edu/buffalo/cse/blue/recordreplay/MainActivity.java
@@ -1,10 +1,17 @@
 package edu.buffalo.cse.blue.recordreplay;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	private String TAG = "REC";
+	
+	private boolean recording = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,21 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    /**
+     * Record the user's location until they stop recording.
+     * @param view the Record Button pressed.
+     */
+    public void recordLocation(View view) {
+    	Button recordButton = (Button)view;
+    	recording = !recording;
+    	Log.v(TAG, "Recording: " + recording);
+    	if(recording) {
+    		recordButton.setText(R.string.stop_record);
+    	} else {
+    		recordButton.setText(R.string.record);
+    	}
     }
     
 }
