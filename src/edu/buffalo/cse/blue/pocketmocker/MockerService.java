c8fdd95a250adc81722c5eac468a0db53667d390
Nick DiRienzo <nvdirien@buffalo.edu>
2014-01-24 04:33:56 -0500

    Woo, have a service that I can send messages to.

--- src/edu/buffalo/cse/blue/pocketmocker/MockerService.java
+++ src/edu/buffalo/cse/blue/pocketmocker/MockerService.java
@@ -7,19 +7,23 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.util.Log;
 import android.widget.Toast;
 import edu.buffalo.cse.blue.pocketmocker.models.MockLocation;
 import edu.buffalo.cse.blue.pocketmocker.models.MockLocationManager;
 
 public class MockerService extends Service {
-	
+
+	public static final String TAG = "MOCK_SRV";
+
 	private final Messenger messenger = new Messenger(new IncomingHandler());
 	private PocketMockerApplication app;
 	private MockLocationManager mockLocationManager;
-	
+
 	@Override
 	public void onCreate() {
-		Toast.makeText(getApplicationContext(), "Mocker service started.", Toast.LENGTH_LONG).show();	
+		Toast.makeText(getApplicationContext(), "Mocker service started.", Toast.LENGTH_LONG)
+				.show();
 		mockLocationManager = MockLocationManager.getInstance(getApplicationContext());
 		app = (PocketMockerApplication) getApplicationContext();
 	}
@@ -28,18 +32,20 @@ public class MockerService extends Service {
 	public IBinder onBind(Intent arg0) {
 		return messenger.getBinder();
 	}
-	
+
 	@SuppressLint("HandlerLeak")
 	class IncomingHandler extends Handler {
 		@Override
 		public void handleMessage(Message msg) {
-			Toast.makeText(app, "MSG RECEIVED", Toast.LENGTH_LONG).show();
+			Log.v(TAG, "Message received: " + msg.toString());
 			if (app.isReplaying()) {
 				MockLocation mockLoc = mockLocationManager.getNext();
 				if (mockLoc == null) {
 					app.setIsReplaying(app.DO_NOT_REPLAY);
 				} else {
-					Toast.makeText(app, app.buildLocationDisplayString(mockLoc.getRealLocation()), Toast.LENGTH_LONG).show();
+					Log.v(TAG,
+							"Location to return: "
+									+ app.buildLocationDisplayString(mockLoc.getRealLocation()));
 				}
 			}
 			super.handleMessage(msg);
---
+++ src/edu/buffalo/cse/blue/pocketmocker/TestMockerServiceActivity.java
@@ -0,0 +1,92 @@
+package edu.buffalo.cse.blue.pocketmocker;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.View;
+
+public class TestMockerServiceActivity extends Activity {
+
+	private final String TAG = MockerService.TAG;
+	private Messenger messengerService = null;
+	private boolean isServiceBound;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.test_mocker_service_activity);
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		this.doUnbindService();
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		this.doBindService();
+	}
+
+	public void sendMessage(View view) {
+		Log.v(TAG, "Sending message");
+		Message msg = Message.obtain();
+		Bundle data = new Bundle();
+		data.putString("MSG", "YOSUPDUDE");
+		msg.setData(data);
+		try {
+			messengerService.send(msg);
+			Log.v(TAG, "Message sent.");
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+
+	class IncomingHandler extends Handler {
+		@Override
+		public void handleMessage(Message msg) {
+			Log.v(TAG, msg.toString());
+		}
+	}
+
+	private final Messenger messenger = new Messenger(new IncomingHandler());
+
+	private ServiceConnection connection = new ServiceConnection() {
+
+		@Override
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			messengerService = new Messenger(service);
+			Log.v(TAG, "Connected to mocker service.");
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName arg0) {
+			messengerService = null;
+			Log.v(TAG, "Mocker service crashed. Disconnecting.");
+		}
+
+	};
+
+	private void doBindService() {
+		this.bindService(new Intent(TestMockerServiceActivity.this, MockerService.class),
+				connection, Context.BIND_AUTO_CREATE);
+		isServiceBound = true;
+		Log.v(TAG, "Binding to mocker service");
+	}
+	
+	private void doUnbindService() {
+		this.unbindService(connection);
+		isServiceBound = false;
+		Log.v(TAG, "Unbound from mocker service.");
+	}
+}
