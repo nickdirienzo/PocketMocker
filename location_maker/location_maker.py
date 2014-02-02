1dda274617419419991b4fd4a332c1918542531f
Nick DiRienzo <me@nickdirienzo.com>
2014-02-01 22:54:43 -0500

    And we have more realistic movements.

--- location_maker/location_maker.py
+++ location_maker/location_maker.py
@@ -10,13 +10,15 @@ PM_DB_PATH = '/data/data/edu.buffalo.cse.blue.pocketmocker/databases/PocketMocke
 LAT_INDEX = 0
 LONG_INDEX = 1
 ALT_INDEX = 1
-OUTSIDE_DAVIS_WALK_ID = 2
+OUTSIDE_DAVIS_WALK_ID = 1
+SECONDS = 15
+
 current_id = os.system(' '.join(['adb shell', '"echo \'select count(*) from locations;\' | sqlite3 ' + PM_DB_PATH + '"']))
 
 class MockLocation(object):
-    def __init__(self, _id, lat, lon, alt):
+    def __init__(self, _id, lat, lon, alt, time_shift):
         self._id = _id
-        ts = time.time()
+        ts = time.time() + time_shift
         self.creation_date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
         self.rec_id = OUTSIDE_DAVIS_WALK_ID
         self.timestamp = ts
@@ -45,14 +47,16 @@ args = parser.parse_args()
 with open(args.source, 'r') as waypoints:
     dom = xml.dom.minidom.parse(waypoints)
     waypoints = dom.getElementsByTagName('wpt')
+    time_shift = 0
     for index, waypoint in enumerate(waypoints):
         current_id += 1
+        time_shift += 1
         lat = waypoint.attributes.item(LAT_INDEX).value
         lon = waypoint.attributes.item(LONG_INDEX).value
         # This whole script is a hack anyway
         alt = waypoint.childNodes[ALT_INDEX].firstChild.data
         print '%s/%s Lat: %s Long: %s Altitude: %s' % (index + 1, len(waypoints), lat, lon, alt)
-        mock = MockLocation(current_id, lat, lon, alt)
+        mock = MockLocation(current_id, lat, lon, alt, time_shift * SECONDS)
         values = ', '.join([str(item) for item in mock.get_fields()])
         sql = 'insert into locations values (%s);' % values
         cmd = '"echo \'%s\' | sqlite3 %s"' % (sql, PM_DB_PATH)
